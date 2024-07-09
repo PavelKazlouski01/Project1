12d3a9116fa818706d1fa00c82f4143c4aae372a
PavelKazlouski01 <Kazlouski.p@gmail.com>
2024-07-09 23:12:14 +0300

    modified:   src/Content.css 	modified:   src/Content.js

--- src/Content.js
+++ src/Content.js
@@ -9,11 +9,13 @@ const Content = () => {
             <div>
                 <h1>Anomalies</h1>
                 <h2>Funnel</h2>
-                <p>An anomaly of presumably gravitational nature.
+                <p>
+                    An anomaly of presumably gravitational nature.
                     At the moment of activation, with terrible force it draws into itself everything that is within a radius of 10-15 meters.
                     If you get into the center of the “funnel,” there is no chance of survival: the body of both the animal and the person will be compressed into a dense lump, and then torn apart at the moment of the so-called discharge.
                     During the entire period of existence - on average about a week - the anomaly does not change the place of its manifestation; can be visually detected during the day by the characteristic air movement above it, swirling foliage, fragments of dismembered corpses and a characteristic dark spot on the ground in the center.
-                    It is extremely dangerous at night because it can only be detected by detectors or by throwing metal objects. </p>
+                    It is extremely dangerous at night because it can only be detected by detectors or by throwing metal objects.
+                </p>
 
                 <p>
                     <img src="https://pm1.aminoapps.com/7780/b34625cb99c9f8089d3c668eea8dc983b23fcce9r1-989-741v2_uhq.jpg" alt="Funnel.pics" />
@@ -21,27 +23,33 @@ const Content = () => {
             </div>
             <div>
                 <h2>Frying</h2>
-                <p>In an inactive state, it looks like a barely visible cloud of hot air, but when any object or living creature enters the action zone, it forms a compact zone heated to a temperature of about 1500K.
-                    At night it can only be detected by powerful detectors or by throwing metal objects.</p>
+                <p>
+                    In an inactive state, it looks like a barely visible cloud of hot air, but when any object or living creature enters the action zone, it forms a compact zone heated to a temperature of about 1500K.
+                    At night it can only be detected by powerful detectors or by throwing metal objects.
+                </p>
                 <p>
                     <img src="https://cs9.pikabu.ru/post_img/big/2016/10/21/6/1477042508150876970.jpg" alt="Frying" />
                 </p>
             </div>
             <div>
                 <h2>Aspic</h2>
-                <p>Education is not completely clear nature.
+                <p>
+                    Education is not completely clear nature.
                     It looks like a glowing bright green jelly, clearly visible in the dark.
-                    Upon contact with the body, it causes injuries similar to the effects of strong acid.</p>
+                    Upon contact with the body, it causes injuries similar to the effects of strong acid.
+                </p>
                 <p>
                     <img src="https://avatars.dzeninfra.ru/get-zen_doc/3725151/pub_5feac085adb1796a08a5beae_5feaccddadb1796a08b3053c/scale_1200" alt="Aspic" />
                 </p>
             </div>
             <div>
                 <h2>Electra</h2>
-                <p>An anomalous formation with a diameter of about 10 meters that accumulates static electricity.
+                <p>
+                    An anomalous formation with a diameter of about 10 meters that accumulates static electricity.
                     A disturbed anomaly explodes with dozens of mini-lightning bolts, and electric shock is almost always fatal for any living creature.
                     A characteristic feature of the Electra is the bluish fog visible above it during the day.
-                    At night it is easily detected by any kind of detectors or by throwing metal objects.</p>
+                    At night it is easily detected by any kind of detectors or by throwing metal objects.
+                </p>
                 <p>
                     <img src="https://kartinki.pics/uploads/posts/2022-12/thumbs/1671747299_kartinkin-net-p-kartinki-iz-igri-stalker-instagram-2.jpg" alt="Electra" />
                 </p>
@@ -54,7 +62,22 @@ const Content = () => {
                     All artifacts of this anomaly, when worn on a belt, affect a person’s resistance to radiation due to the strong absorbent properties of down.
                 </p>
                 <p>
-                    <img scr="https://static.wikia.nocookie.net/stalker_ru_gamepedia/images/7/7c/Ui_icon_puh.png/revision/latest?cb=20191116124434" alt="Burning fluff" />
+                    <img src="https://stalker-epos.com/zp/anomals/epos-zp-zguchiy-puh.jpg" alt="Burning fluff" />
+                </p>
+            </div>
+            <div>
+                <h2>Springboard</h2>
+                <p>
+                    This anomaly was one of the first to be discovered back in 2008.
+                    It causes impact damage with a gravitational field, similar to a shock wave from an explosion.
+                    “Lives” in one place for an average week. Over the course of its life, the force of influence changes.
+                    The force of impact ranges from bruises and contusions to instant death with the launch of the body into orbit.
+                    It is detected by distortions in the air above it, swirling foliage, as well as characteristic red-brown spots on the ground that are the “starts” of losers and newbie dunces.
+                    Its influence on a person can be slightly reduced by wearing the artifacts "Twist", "Gravity" and "Goldfish".
+                    It is easily detected by all types of detectors and by throwing various metal objects at it.
+                </p>
+                <p>
+                    <img src="https://i.ytimg.com/vi/a_dOkhuKPAc/maxresdefault.jpg" alt="Springboard" />
                 </p>
             </div>
         </div>
